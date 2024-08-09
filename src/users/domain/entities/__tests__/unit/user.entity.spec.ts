0552b313a93f82a6902c180ffd57e8e4f4fa2381
Alexandre <alexandre.bacellar@gmail.com>
2024-08-09 04:05:11 -0300

    test: Getters of User Entity unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -24,4 +24,27 @@ describe('User unit tests', () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   })
+
+  it('Getters of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toEqual('string');
+  })
+
+  it('Getters of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toEqual('string');
+  })
+
+  it('Getters of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toEqual('string');
+  })
+
+  it('Getters of createdAt field', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  })
 })
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -13,4 +13,20 @@ export class User {
     //Set date to now if none is provided.
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
 }
